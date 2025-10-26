bb2c34b3daf8715f24c520328f16bbb9e8d4d8e0
Renvel19 <af.renteria2@uniandes.edu.co>
2025-10-26 12:12:00 -0500

    remove unused imports

--- app/src/main/java/com/example/medisupplyapp/screen/visits/RegisterVisitScreen.kt
+++ app/src/main/java/com/example/medisupplyapp/screen/visits/RegisterVisitScreen.kt
@@ -1,21 +1,13 @@
 package com.example.medisupplyapp.screen.visits
 
-import androidx.compose.foundation.background
-import androidx.compose.foundation.border
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.interaction.MutableInteractionSource
+
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.CalendarToday
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -30,9 +22,6 @@ import com.example.medisupplyapp.components.SimpleTopBar
 import com.example.medisupplyapp.components.ToastType
 import com.tuempresa.medisupply.ui.components.FooterNavigation
 import com.tuempresa.medisupply.ui.theme.MediSupplyTheme
-import java.text.SimpleDateFormat
-import java.util.Date
-import java.util.Locale
 
 
 @Composable
@@ -101,7 +90,7 @@ fun RegisterVisitScreen(
                         onClick = {
                             if (viewModel.isFormValid()) {
                                 viewModel.registerVisit(
-                                    onSuccess = { _, message ->
+                                    onSuccess = { _, _ ->
                                         toastMessage = successMessage
                                         toastType = ToastType.SUCCESS
                                         showToast = true
