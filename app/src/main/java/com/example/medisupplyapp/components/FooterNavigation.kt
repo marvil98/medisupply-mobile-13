d4882f21ea0488cc15f572bf4e003c1f914f6f3e
Renvel19 <af.renteria2@uniandes.edu.co>
2025-11-16 10:41:16 -0500

    fix display of FooterNavigation.kt

--- app/src/main/java/com/example/medisupplyapp/components/FooterNavigation.kt
+++ app/src/main/java/com/example/medisupplyapp/components/FooterNavigation.kt
@@ -5,13 +5,18 @@ import androidx.compose.material.icons.filled.*
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.graphics.*
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.*
 import com.example.medisupplyapp.R
+import com.example.medisupplyapp.data.provider.authCacheDataStore
 import com.example.medisupplyapp.data.remote.ApiConnection
 import com.example.medisupplyapp.data.remote.repository.ClientRepository
+import com.example.medisupplyapp.datastore.AuthCacheProto
 
 @Composable
 fun FooterNavigation(
@@ -19,11 +24,10 @@ fun FooterNavigation(
     onNavigate: (String) -> Unit
 ) {
     val context = LocalContext.current
-    val userRepository = remember { ClientRepository(ApiConnection.api_users,context) }
-    var role = ""
-    LaunchedEffect(Unit) {
-       role = userRepository.getUserRole()
-    }
+    val authData by context.authCacheDataStore.data.collectAsState(
+        initial = AuthCacheProto.getDefaultInstance()
+    )
+    val role = authData.role
     NavigationBar(
         containerColor = MaterialTheme.colorScheme.primary
     ) {
